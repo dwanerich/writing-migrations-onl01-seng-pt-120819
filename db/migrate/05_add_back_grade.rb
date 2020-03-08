7cd7b4bb81a8f89b62afae35f49d7f28feee8f01
dwane richards <dwanerich@gmail.com>
2020-03-08 17:13:11 +0000

    Automatically backed up by Learn

--- db/migrate/05_add_back_grade.rb
+++
@@ -1,8 +0,0 @@
-class AddBackGrade < ActiveRecord::Migration[5.1]
-
-  def change
-    add_column :honor_roll, :grade, :integer
-  end
-end
-
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "db/artists.sqlite")
