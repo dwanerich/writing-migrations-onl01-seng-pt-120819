39a3e481c7f3a76ccb7d43c0475bac963605bcd6
dwane richards <dwanerich@gmail.com>
2020-03-08 17:05:57 +0000

    Automatically backed up by Learn

--- db/migrate/04_remove_error_column.rb
+++ db/migrate/04_remove_error_column.rb
@@ -4,3 +4,5 @@ class RemoveErrorColumn < ActiveRecord::Migration[5.1]
     remove_column :honor_roll, :grade, :datetime
   end
 end
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "db/artists.sqlite")
